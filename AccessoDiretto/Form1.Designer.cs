c39ca1372956f77d13c9f431d509a26f0c8ab8f1
DomeManca <83721834+DomeManca@users.noreply.github.com>
2022-10-02 18:39:35 +0200

    Compito finito

---
+++ AccessoDiretto/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace AccessoDiretto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::AccessoDiretto.Properties.Resources.Trivago;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 121);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 139);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inserire comune";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(335, 190);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&Cerca";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 245);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(322, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Il primo hotel disponibile è: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(16, 190);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 36);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "della provincia di verona";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 283);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(420, 77);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "|";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 382);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Trivago";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AccessoDiretto/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccessoDiretto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("info");
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            int na = a.Length;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = (Ricerca("veneto_verona.csv", textBox1.Text));
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.Text;
+        }
+        static string Ricerca(string filename, string loc)
+        {
+            string a = "";
+            var file = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite);// accesso al file binario 
+            BinaryReader reader = new BinaryReader(file);
+            BinaryWriter writer = new BinaryWriter(file);
+            int righetot = Convert.ToInt32(file.Length);//byte totali
+            int lunghezzariga = 528;
+            righetot /= 528; // per ottenere il numero di righe 
+
+            string result = "";
+
+            int lung = Convert.ToInt32(file.Length);
+            int i = 0, j = righetot - 1, m, pos = -1;
+
+            do // RICERCA DICOTOMICA
+            {
+                m = (i + j) / 2;
+                file.Seek(m * lunghezzariga, SeekOrigin.Begin);
+                a = Encoding.ASCII.GetString(reader.ReadBytes(lunghezzariga));
+                result = FromString(a, 0);
+
+                if (myCompare(result, loc) == 0)
+                {
+                    pos = m;
+                }
+                else if (myCompare(result, loc) == -1)
+                {
+                    i = m + 1;
+                }
+                else
+                    j = m - 1;
+
+
+            } while (i <= j && pos == -1);
+
+            string fine;
+            if (pos != -1)
+            { 
+                fine = FromString(a, 7);
+            }
+            else
+            {
+                MessageBox.Show("Non esiste questa località");
+                fine = "|";
+            }
+            
+            file.Close();
+            return fine;
+
+        }
+        static int myCompare(string stringa1, string stringa2)
+        {
+            if (stringa1 == stringa2)//0=sono uguali 1=stringa viene prima -1=stringa viene dopo
+                return 0;
+
+            char[] char1 = stringa1.ToCharArray();
+            char[] char2 = stringa2.ToCharArray();
+            int l = char1.Length;
+            if (char2.Length < l)//in l c'è la lunghezza più piccola
+                l = char2.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                if (char1[i] < char2[i])
+                    return -1;
+                if (char1[i] > char2[i])
+                    return 1;
+            }
+
+            return 0;//visto che qui non mi interessa lunghezza allora mi basta che la prima parte si uguale
+        }
+        public static string FromString(string Stringa, int pos, string sep = ";")//funzione che da una stringa separa i campi e ritorna una stringa
+        {
+            string[] pulito = Stringa.Split(';');
+            return pulito[pos];
+        }
+    }
+}
